c31e44cc0682a77c294de3e465c978940453c87c
Tobi Adesokan <70437868+tobisamcode@users.noreply.github.com>
2025-03-24 08:13:58 +0100

    # Count Unique Characters in a String

---
+++ 10 - Count Unique Characters in a String/solution.py
@@ -0,0 +1,7 @@
+def count_unique_characters(string):
+    # Your code here
+    unique_chars = set(string)
+    return len(unique_chars)
+
+# Test Cases
+print(count_unique_characters("Hello World"))
