1cb2dbbc3ec6f6c2953d23590b725479d84ad9b2
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-06 14:19:20 +0100

    # Determine the Longest Sequence of Consecutive Ones in a Binary Array

---
+++ 21 - Determine the Longest Sequence of Consecutive Ones in a Binary Array/solution.py
@@ -0,0 +1,18 @@
+def find_longest_sequence(nums):
+    # Your code here
+    max = 0
+    count = 0
+
+    for num in nums:
+        if num == 1:
+            count += 1
+            if count > max:
+                max = count
+        else:
+            count = 0
+
+    return max
+
+
+#   Test cases
+print(find_longest_sequence([1, 1, 0, 1, 1, 1]))
