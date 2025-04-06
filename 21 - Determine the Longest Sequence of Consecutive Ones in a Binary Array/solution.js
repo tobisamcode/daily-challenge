51fbdede5f7bdd4ae0f0942731cba8cc671f6e11
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-06 14:19:16 +0100

    # Determine the Longest Sequence of Consecutive Ones in a Binary Array

---
+++ 21 - Determine the Longest Sequence of Consecutive Ones in a Binary Array/solution.js
@@ -0,0 +1,21 @@
+function findLongestSequence(nums) {
+  // Your code here
+  let max = 0;
+  let count = 0;
+
+  for (let num of nums) {
+    if (num === 1) {
+      count++;
+      if (count > max) {
+        max = count;
+      }
+    } else {
+      count = 0;
+    }
+  }
+
+  return max;
+}
+
+//   Test cases
+console.log(findLongestSequence([1, 1, 0, 1, 1, 1]));
