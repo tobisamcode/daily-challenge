95f058f8070569395d0a1073c74c1ccf227797c0
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-16 14:27:56 +0100

    # Find the Second Largest Number in an Array

---
+++ 30 - Find the Second Largest Number in an Array/solution.js
@@ -0,0 +1,18 @@
+function findSecondLargest(arr) {
+  let first = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > first) {
+      second = first;
+      first = num;
+    } else if (num > second && num != first) {
+      second = num;
+    }
+  }
+
+  return second === -Infinity ? null : second;
+}
+
+// Test Cases
+console.log(findSecondLargest([5, 3, 9, 1, 9])); // 5
