315c1df5d10c1b9c32ae2b176d0bd81886e2805d
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-20 23:34:24 +0100

    # Merge Overlapping Time Intervals

---
+++ 35 - Merge Overlapping Time Intervals/solution.py
@@ -0,0 +1,21 @@
+def merge_intervals(intervals):
+    if not intervals:
+        return []
+    
+    intervals.sort(key=lambda x: x[0])
+
+    merged = [intervals[0]]
+
+    for i in range(1, len(intervals)):
+        last = merged[-1]
+        current = intervals[i]
+
+        if current[0] <= last[1]:
+            last[1] = max(last[1], current[1])
+        else:
+            merged.append(current)
+
+    return merged
+
+# Test Cases
+print(merge_intervals([[1, 3], [2, 6], [8, 10], [15, 18]])) 
