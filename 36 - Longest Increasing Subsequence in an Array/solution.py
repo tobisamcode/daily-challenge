5b2d23d72e8aedb06991da1f71d6f0bd27dcaef4
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-21 01:17:17 +0100

    # Longest Increasing Subsequence in an Array

---
+++ 36 - Longest Increasing Subsequence in an Array/solution.py
@@ -0,0 +1,16 @@
+def length_of_lis(nums):
+    if not nums:
+        return 0
+
+    dp = [1] * len(nums)
+
+    for i in range(1, len(nums)):
+        for j in range(0, i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp)
+
+
+# Test Cases
+print(length_of_lis([10, 9, 2, 5, 3, 7, 101, 18]))
